9fcdea39ed126c361c2f1fbf7a06785a0030a336
Martin Chung <Mchung29@connect.kellettschool.com>
2026-03-29 17:22:47 +0800

    added prison system and movement system around the board accounting for doubles

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
 
 def startup(): ##setup before game starts
     clear_screen()
-    ##bgm() 
+    #bgm() 
     intro()
     game_style = bot_player()
     if game_style == "multiplayer":
@@ -120,9 +120,10 @@ def var_assign(playercount): ## character and name selection
                         "icon" : chosen_set[0],
                         "color" : chosen_set[1],
                         "properties" : [],
-                        "jail" : False,
-                        "jail_card" : True,
-                        "own_tr_stat" : 0
+                        "jail" : True,
+                        "jail_cards" : 1,
+                        "jail_turns" : 0,
+                        "own_train_stats" : 0
                     }
                     players_data.append(pdata)
                     icon_valid = True
@@ -143,50 +144,112 @@ def var_assign(playercount): ## character and name selection
 
 
 def game(players_data, playerlist, playercount):
-    board_data()
+    board_data = board_dinit()
     game_running = True     ## will change when I implement checking for no remaining players
-    check_win(playerlist)
     while game_running == True:
-        for i in range(0, playercount):
-            turn_active = True
-            while turn_active == True:
-                curr_pdata = players_data[i]
-                curr_pname = playerlist[i]
-                turn(curr_pname, playercount, curr_pdata)
-                if doubles == False:
-                    turn_active == False
+        for i in range(playercount):
+            curr_pdata = players_data[i]
+            curr_pname = playerlist[i]
+            turn(curr_pname, playercount, curr_pdata, board_data)
+            check_win(playerlist)
 
-
-def turn(curr_pname, playercount, curr_pdata): ##process of a turn
-    printboard()
-    your_turn(curr_pname)
-    rollnum, doubles = roll()
-    move(rollnum)
-    check_bankrupt(curr_pdata)
-    return doubles
+def turn(curr_pname, playercount, curr_pdata, board_data): ##process of a turn
+    doubles_count = 0
+    turn_active = True
+    while turn_active == True:
+        clear_screen()
+        printboard()
+        if curr_pdata["jail"] == True:
+            jail_turn(curr_pdata, board_data)
+            break
+        your_turn(curr_pname)
+        is_doubles, roll_sum = roll()
+        if is_doubles == True:
+            doubles_count += 1
+            if doubles_count == 3:
+                orange("You are SPEEDING! Go to jail.")
+                prison(curr_pdata)
+                break
+        move(roll_sum, curr_pdata, board_data)
+        check_bankrupt(curr_pdata)
+        if is_doubles == False:
+            turn_active = False
 
 def your_turn(curr_pname):
     lime(f"{curr_pname}, it's your turn!")
 
 def roll(): ## getting numbers from rolling 2 dice
-    magenta("Press enter to roll your dice")
+    magenta("Press enter to roll your dice.")
     input()
+    slow_print("Rolling...")
+    time.sleep(0.3)
     roll_num = random.randint(1,6)
     roll_num2 = random.randint(1,6)
     roll_total = roll_num + roll_num2
     green(f"You rolled a {roll_num} and a {roll_num2}!")
+    time.sleep(1)
     if roll_num == roll_num2:
         green("🄳 🄾 🅄 🄱 🄻 🄴 🅂 !")
-        doubles = True
+        is_doubles = True
     else:
-        doubles = False
-    return doubles, roll_total
+        is_doubles = False
+    return is_doubles, roll_total
 
-def prison(): ##sends someone to prison instantly
-    pass
+def prison(curr_pdata): ##sends someone to prison instantly
+    red("You do not pass GO or collect 200$.")
+    time.sleep(1)
+    slow_print("You are now in prison. You can either roll for doubles or use a jail free card if you have any.")
+    time.sleep(1)
+    curr_pdata["position"] = 10
+    curr_pdata["jail"] = True
 
-def move(): ## moves someone to a tile on the board
-    old_pos = player["position"]
+def jail_turn(curr_pdata, board_data):
+    slow_print(f"{curr_pdata["name"]}, it's your JAIL turn!")
+    if curr_pdata["jail_cards"] > 0:
+        gold(f"You have {curr_pdata["jail_cards"]} Get out of jail free card(s).")
+        blue("You can type 'use' to use a card. Otherwise, roll doubles py pressing enter.")
+        choice = input().lower()
+        clear_screen()
+        printboard()
+        if choice == "use":
+            curr_pdata["jail_cards"] -= 1
+            curr_pdata["jail"] = False
+            curr_pdata["jail_turns"] = 0
+            green("Card used! You are free to move!")
+            turn()
+            return
+    ## if no card:
+    is_doubles, roll_total = roll()
+    if is_doubles == True:
+        green("Freedom! You rolled doubles!")
+        curr_pdata["jail"] = False
+        curr_pdata["jail_turns"] = 0
+        move(roll_total, curr_pdata, board_data)
+    else:
+        curr_pdata["jail_turns"] += 1
+        red(f"No doubles. You have spent {curr_pdata["jail_turns"]}/3 turns in jail.")
+        if curr_pdata["jail_turns"] < 3:
+            blue("Press enter to proceed.")
+            input()
+        else:
+            red("You have spent 3 turns in jail. Paying 50$ to leave.")
+            curr_pdata["money"] -= 50
+            curr_pdata["jail"] = False
+            curr_pdata["jail_turns"] = 0
+            move(roll_total, curr_pdata, board_data)
+
+
+def move(roll_sum, curr_pdata, board_data): ## moves someone to a tile on the board
+    old_pos = curr_pdata["position"]
+    new_pos = (old_pos + roll_sum) % 40
+    curr_pdata["position"] = new_pos
+    curr_tile = board_data[new_pos]
+    tile_name = curr_tile["name"]
+    blue(f"You are now on {tile_name}! Press enter to proceed.")
+    input()
+    if curr_pdata["position"] < old_pos:
+        print("You passed GO! Collect 200$")
+        curr_pdata["money"] += 200
 
 def prop_tile(): ## what happens when you land on a property tile
     owned = check_owned()
@@ -204,10 +267,21 @@ def check_owned(): ## checks if a property tile is owned
 def botactions():
     pass
 
+def check_win(playerlist):
+    if len(playerlist) == 1:
+        win = True
+        return win
+    else:
+        pass
+    pass
+
+def check_bankrupt(players_data):
+    pass
+
 def printboard():
-    print("""
+    green("""
 _______________________________________________________________________________________________________
-|   Free   | Rathore| Chance |   Fu   | Bidoof | Farnham| Dream |  Fox   |  Water |Fairplay|  Go to   |
+|   Free   | Rathore| Chance |   Fu   | Bidoof | Farnham| Dream  |  Fox   |  Water |Fairplay|  Go to   |
 |  Parking |  Park  |   ❓   |   St.  | Valley | 🚂 Sta.| Island |   St.  | Works💧|  Ave.  |   JAIL   |
 |    🚗    |        |        |        |        |        |        |        |        |        |   🚔🚨   |
 |          |  220$  |        |  220$  |  240$  |  200$  |  260$  |  260$  |  150$  |  280$  |          |
@@ -255,17 +329,8 @@ ________________________________________________________________________________
 |__________|________|________|________|________|________|________|________|________|________|__________|
 """)
 
-def check_win(playerlist):
-    if len(playerlist) == 1:
-        win = True
-        return win
-    else:
-        pass
-    pass
-def check_bankrupt(players_data):
-    pass
-def board_data():
-    monopoly_board = [
+def board_dinit():
+    tile_data = [
     {"id": 0, "name": "GO", "type": "special", "owner": None, "houses": 0},
     {"id": 1, "name": "Paper Street", "type": "property", "price": 60, "owner": None, "houses": 0},
     {"id": 2, "name": "Community Chest (South)", "type": "card", "owner": None, "houses": 0},
@@ -290,7 +355,7 @@ def board_data():
     {"id": 21, "name": "Rathore Park", "type": "property", "price": 220, "owner": None, "houses": 0},
     {"id": 22, "name": "Chance (North)", "type": "card", "owner": None, "houses": 0},
     {"id": 23, "name": "Fu Street", "type": "property", "price": 220, "owner": None, "houses": 0},
-    {"id": 24, "name": "Bidoor Valley", "type": "property", "price": 240, "owner": None, "houses": 0},
+    {"id": 24, "name": "Bidoof Valley", "type": "property", "price": 240, "owner": None, "houses": 0},
     {"id": 25, "name": "Fanham Station", "type": "property", "price": 200, "owner": None, "houses": 0},
     {"id": 26, "name": "Dream Island", "type": "property", "price": 260, "owner": None, "houses": 0},
     {"id": 27, "name": "Fox Street", "type": "property", "price": 260, "owner": None, "houses": 0},
@@ -307,13 +372,13 @@ def board_data():
     {"id": 38, "name": "Luxury Tax", "type": "tax", "price": None, "owner": None, "houses": 0},
     {"id": 39, "name": "Langley Incorporated", "type": "property", "price": 400, "owner": None, "houses": 0}
 ]
-
+    return tile_data
     
 ## Text-related stuff
 def slow_print(text): ##slow typing effect
     for char in text:
         print(char, end='', flush=True)
-        time.sleep(0.025)
+        time.sleep(0.04)
     print()
 
 def clear_screen():
@@ -346,4 +411,5 @@ def magenta(string):
 
 def green(string):
     rgb(string, 29, 219, 73)
-main()
+
+##main()
